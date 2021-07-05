80659a0fed426704be49a33874420125fdaa539b
dragonregure <dragonregure@gmail.com>
2021-07-05 12:47:03 +0700

    lesson-2-typography

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Typography } from '@material-ui/core';
 
 export default function Create() {
   return (
     <div>
-      Create page
+      <Typography
+        variant="h6"
+        color="textSecondary"
+        gutterBottom
+      >
+        Create a new notes
+      </Typography>
+
+      <Typography
+        noWrap
+      >
+        Aliqua duis consequat nostrud nostrud ullamco duis elit et Lorem excepteur nisi. Laborum labore reprehenderit velit Lorem esse fugiat laboris deserunt quis nisi eiusmod aliquip ipsum labore. Cillum sit anim excepteur occaecat nulla officia esse Lorem. Amet ea consequat excepteur aliqua sint nostrud culpa et fugiat occaecat. Ut velit qui consectetur reprehenderit qui officia proident nulla sint dolore adipisicing consequat do.
+      </Typography>
     </div>
   )
 }
